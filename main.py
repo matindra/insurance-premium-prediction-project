6913e46e338e408466f77ad8eb551504697569d2
matindra <matindramalik@gmail.com>
2023-05-22 10:14:09 +0530

    data_transformation completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from insurance.utils import get_collection_as_dataframe
 from insurance.entity import config_entity
 from insurance.components.data_ingestion import DataIngestion
 from insurance.components.data_validation import DataValidation
+from insurance.components.data_transformation import DataTransformation
 
 # def test_logger_and_exception():
 #     try:
@@ -35,6 +36,13 @@ if __name__=="__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
         
         data_validation_artifact = data_validation.initiate_data_validation()
+
+      #Data Transformation
+
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+        data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
 
     except Exception as e:
         print(e)
